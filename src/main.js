3ef0f41f9789d4c5f2faa6beae6d0d6dbebfe070
Luigi Frunzio <frunzl@usi.ch>
2016-12-22 16:04:36 +0100

    Setup :)

---
+++ src/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Giggiux on 22-Dec-16.
+ */
+
+function ciaone(prova){
+    return console.log(prova);
+}
+
+ciaone("lol");
+
+async function hello (wei) {
+    await ciaone("trollolol");
+}
+
+hello();
